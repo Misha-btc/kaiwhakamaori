1fc1425d13e942e389208ab6cc62bc264cf28f02
misha <tuskar.israel@gmail.com>
2024-10-19 16:52:03 -0500

    Add text translation to English feature

--- content.js
+++ content.js
@@ -5,32 +5,35 @@ chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
     
     const activeElement = document.activeElement;
 
-    if (activeElement && (activeElement.tagName === "INPUT" || activeElement.tagName === "TEXTAREA")) {
-      const inputContent = activeElement.value;
-
-      chrome.runtime.sendMessage(
-        { type: "GENERATE_TEXT", prompt: inputContent },
-        (response) => {
-          if (chrome.runtime.lastError) {
-            console.error("Ошибка при отправке сообщения:", chrome.runtime.lastError);
-            updateInputValue(activeElement, "Hello " + inputContent);
-          } else if (response && response.success) {
-            updateInputValue(activeElement, "Hello " + response.data);
-          } else {
-            console.error("Неожиданный ответ:", response);
-            updateInputValue(activeElement, "Hello " + inputContent);
-          }
+    if (activeElement && (activeElement.tagName === "INPUT" || activeElement.tagName === "TEXTAREA" || activeElement.isContentEditable)) {
+      const inputContent = getElementContent(activeElement);
+      chrome.runtime.sendMessage({type: "FIX_PUNCTUATION", text: inputContent}, (response) => {
+        if (response.success) {
+          updateElementContent(activeElement, response.fixedText);
+        } else {
+          console.error("Ошибка при исправлении пунктуации:", response.error);
         }
-      );
-    } else {
-      console.log("Активный элемент не является текстовым полем или текстовой областью");
+      });
     }
   }
 });
 
-function updateInputValue(element, value) {
-  element.value = value;
-  element.dispatchEvent(new Event('input', { bubbles: true }));
-  element.dispatchEvent(new Event('change', { bubbles: true }));
+function getElementContent(element) {
+  if (element.isContentEditable) {
+    return element.innerText;
+  }
+  return element.value;
+}
+
+function updateElementContent(element, value) {
+  if (element.isContentEditable) {
+    element.innerText = value;
+    element.dispatchEvent(new Event('input', { bubbles: true }));
+    element.dispatchEvent(new Event('change', { bubbles: true }));
+  } else {
+    element.value = value;
+    element.dispatchEvent(new Event('input', { bubbles: true }));
+    element.dispatchEvent(new Event('change', { bubbles: true }));
+  }
   console.log("Содержимое активного элемента изменено на:", value);
 }
